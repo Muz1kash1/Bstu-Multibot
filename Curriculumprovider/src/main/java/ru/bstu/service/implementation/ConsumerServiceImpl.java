4b26d1b872680eceae841e4e81ffa785ce6d576a
Денис Шевченко <Muz1kash1@yandex.ru>
2023-02-09 18:30:41 +0300

    feat: добавил затычку для обработки запроса на расписание

--- Curriculumprovider/src/main/java/ru/bstu/service/implementation/ConsumerServiceImpl.java
+++ Curriculumprovider/src/main/java/ru/bstu/service/implementation/ConsumerServiceImpl.java
@@ -1,13 +1,14 @@
 package ru.bstu.service.implementation;
 
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.bstu.service.ConsumerService;
 import ru.bstu.service.ProducerService;
+import ru.bstu.utils.CurriculumUpdatesProcessor;
 
 import static ru.bstu.commotrabbitmq.RabbitQueue.AUDIO_MESSAGE_UPDATE;
 import static ru.bstu.commotrabbitmq.RabbitQueue.DOC_MESSAGE_UPDATE;
@@ -19,27 +20,24 @@ import static ru.bstu.commotrabbitmq.RabbitQueue.TEXT_MESSAGE_UPDATE;
 @AllArgsConstructor
 public class ConsumerServiceImpl implements ConsumerService {
   private final ProducerService producerService;
+  private final CurriculumUpdatesProcessor curriculumUpdatesProcessor;
 
   /**
    * Принять обновление из очереди текстовых сообщений
+   *
    * @param update обновление
    */
   @Override
+  @SneakyThrows
   @RabbitListener(queues = TEXT_MESSAGE_UPDATE)
   public void consumeTextMessageUpdate(final Update update) {
-    if (update != null) {
-      log.info("получено текстовое сообщение из ребита " + update.getMessage().getText());
-
-      SendMessage sendMessage = new SendMessage();
-      sendMessage.setChatId(update.getMessage().getChatId());
-      sendMessage.setText(update.getMessage().getText() + " но это все получено из ноды");
-      producerService.produceAnswer(sendMessage);
-    }
-
+    producerService.produceAnswer(curriculumUpdatesProcessor.processCurriculumUpdate(update));
   }
+
 
   /**
    * Принять обновление из очереди документов
+   *
    * @param update обновление
    */
   @Override
@@ -50,6 +48,7 @@ public class ConsumerServiceImpl implements ConsumerService {
 
   /**
    * Принять обновление из очереди фотографий
+   *
    * @param update обновление
    */
   @Override
@@ -60,6 +59,7 @@ public class ConsumerServiceImpl implements ConsumerService {
 
   /**
    * Принять обновление из очереди с аудиозаписями
+   *
    * @param update обновление
    */
   @Override
